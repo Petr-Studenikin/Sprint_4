82b12a16776dff8428db0789f6bb6d8175d03ad4
Petr Studenikin <peterstudenickin@yandex.ru>
2024-01-14 22:20:34 +0300

    first

---
+++ sprint_4_final_project-main/src/main/java/ru/yandex/praktikum/pageObject/constants/RentDurationConstants.java
@@ -0,0 +1,12 @@
+package ru.yandex.praktikum.pageObject.constants;
+
+public class RentDurationConstants {
+    public static final String DAY_1 = "сутки";
+    public static final String DAYS_2 = "двое суток";
+    public static final String DAYS_3 = "трое суток";
+    public static final String DAYS_4 = "четверо суток";
+    public static final String DAYS_5 = "пятеро суток";
+    public static final String DAYS_6 = "шестеро суток";
+    public static final String DAYS_7 = "семеро суток";
+
+}
